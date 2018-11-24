808aaa0e617d21ea230a6602507ca1507dbc33ac
zeta <zezeon@msn.com>
2018-11-24 12:21:33 -0300

    --

--- src/hello.cc
+++ src/hello.cc
@@ -1,34 +1,43 @@
 #include <boost/python/suite/indexing/vector_indexing_suite.hpp>
 #include<boost/python.hpp>
-#include"aes.h"
 using namespace boost::python;
 using namespace std;
 
-boost::python::list base64_dec(string s);
-string base64_enc(boost::python::list l);
-string pemtojson(string filename);
-string dertojson(string filename);
-void print(int n);
-struct K
-{
-	void show() { cout << "K" << endl; }
-	int k = 32;
-};
-
-BOOST_PYTHON_MODULE(tls_crypt) {
-	def("base64_encode", &base64_enc);
-	def("base64_decode", &base64_dec);
-	def("pem2json", &pemtojson);
-	def("der2json", &dertojson);
-	def("print", &print);
-	class_<K>("K")
-		.def("show", &K::show)
-		.def_readwrite("k", &K::k)
-		;
-	class_<AES2>("AES2")
-		.def("key", &AES2::key)
-		.def("iv", &AES2::iv)
-		.def("show", &AES2::show)
-		;
+const char *greet() {
+	return "hello world";
 }
 
+vector<int> addv(vector<int> &v) {
+	v.push_back(1);
+	return v;
+}
+struct K {
+	string greet() { return msg; }
+	string msg;
+	vector<int> v = {1, 2};
+	void set(string m) { msg = m; }
+	void addv(vector<int> w) {
+		for(int k : w) v.push_back(k);
+	}
+	void push_back(int k) {
+		v.push_back(k);
+	}
+};
+BOOST_PYTHON_MODULE(hello) {
+	def("greet", greet);
+	def("vadd", addv);
+//	class_<vector<int>>("vecint")
+//		.def("push_back", &vector<int>::push_back)
+//		;
+	class_<vector<int>>("vecz")
+		.def(vector_indexing_suite<vector<int>>())
+		;
+	class_<K>("K")
+		.def("greet", &K::greet)
+		.def("set", &K::set)
+		.def("addv", &K::addv)
+		.def("push_back", &K::push_back)
+		.def_readwrite("msg", &K::msg)
+		.def_readwrite("v", &K::v)
+		;
+}
