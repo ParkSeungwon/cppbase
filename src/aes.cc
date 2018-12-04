726703eba86c03c677a9bd1607b409c5332630df
zeta <zezeon@msn.com>
2018-12-04 19:42:48 -0200

    boost final

--- src/aes.cc
+++ src/aes.cc
@@ -75,3 +75,58 @@ bp::list AES::decrypt2(bp::list enc)
 	auto v = l2v(enc);
 	return v2l(decrypt(v.begin(), v.end()));
 }
+
+DiffieHellman::DiffieHellman(int bit_sz)
+{//server side
+	if(bit_sz == 2048) {
+		p = premade;
+		g = 2;
+	}
+	xa = random_prime(bit_sz / 8);
+	ya = powm(g, xa, p);
+}
+DiffieHellman::DiffieHellman(mpz_class p, mpz_class g, mpz_class ya)
+{//client side
+	this->p = p; this->g = g; this->ya = ya;
+	if(mpz_sizeinbase(p.get_mpz_t(), 16) <= 256) xb = random_prime(128);
+	else xb = random_prime(256);
+	yb = powm(g, xb, p);
+	K = powm(ya, xb, p);
+}
+mpz_class DiffieHellman::set_yb(mpz_class pub_key)
+{//set client pub key
+	yb = pub_key;
+	K = powm(yb, xa, p);
+	return K;
+}
+
+RSA::RSA(int key_size)
+{
+	p = random_prime(key_size / 2);
+	q = random_prime(key_size / 2);
+	K = p * q;
+	phi = lcm(p-1, q-1);//e와 phi는 서로소
+	for(e = 0x10001; gcd(e, phi) != 1; e = nextprime(e));
+	mpz_invert(d.get_mpz_t(), e.get_mpz_t(), phi.get_mpz_t());//modular inverse
+}
+
+RSA::RSA(mpz_class e, mpz_class d, mpz_class K)
+{
+	this->e = e;
+	this->d = d;
+	this->K = K;
+}	
+
+mpz_class RSA::encode(mpz_class m)
+{//K should be bigger than m
+	return powm(m, e, K);
+}
+mpz_class RSA::decode(mpz_class m)
+{
+	return powm(m, d, K);
+}
+mpz_class RSA::sign(mpz_class m)
+{
+	return decode(m);
+}
+
--- src/boost.cc
+++ src/boost.cc
@@ -15,6 +15,8 @@ BOOST_PYTHON_MODULE(tls_crypt) {
 	def("base64_decode", &base64_dec);
 	def("pem2json", &pemtojson);
 	def("der2json", &dertojson);
+	class_<vector<unsigned char>>("vecz")
+		.def(vector_indexing_suite<vector<unsigned char>>());
 	class_<AES>("AES")
 		.def("key", &AES::key2)
 		.def("iv", &AES::iv2)
@@ -23,6 +25,19 @@ BOOST_PYTHON_MODULE(tls_crypt) {
 		.def("decrypt", &AES::decrypt2)
 		;
 	class_<SHA2>("SHA256")
-		.def("hash", &SHA2::hash2)
+		.def("hash", &SHA2::hash2);
+	class_<SHA1>("SHA1")
+		.def("hash", &SHA1::hash2);
+	class_<SHA5>("SHA512")
+		.def("hash", &SHA5::hash2);
+	class_<HMAC<SHA2>>("HMAC")
+		.def("key", &HMAC<SHA2>::key2)
+		.def("hash", &HMAC<SHA2>::hash2)
+		;
+	class_<PRF<SHA2>>("PRF")
+		.def("label", &PRF<SHA2>::label2)
+		.def("seed", &PRF<SHA2>::seed2)
+		.def("secret", &PRF<SHA2>::secret2)
+		.def("get_n_byte", &PRF<SHA2>::get_n_byte)
 		;
 }
